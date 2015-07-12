09dcdaa345ad39273e140efc3fdb2c1bf0f7414d
chanten <chanyeol.park@gmail.com>
2015-07-13 02:20:46 +0900

    Artic(Prim)
    This is not finished

---
+++ Artic(Prim)/Artic(Prim)/artic(prim).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int test_case;
+
+
+int mani()
+{
+	int i, j,samples;
+	freopen("sample_input.txt", "r", stdin);
+
+	scanf("%d", &test_case);
+	scanf("%d", &samples);
+
+	for (i = 0; i < test_case; i++)
+	{
+		for (j = 0; j < samples; j++){
+
+
+
+		}
+
+
+	}
+
+
+
+
+}
